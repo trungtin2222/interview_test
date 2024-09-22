1f4294f48d0433675927b999879054899958cbdf
TinPT <tinpt@HCMHOH02HHUL521.local>
2024-09-22 15:50:40 +0700

    initial

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <Text>{'Interview Test'}</Text>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
